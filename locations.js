a20b09f3b64b1916a8bb6998c784d975da598929
Ben Chatelain <ben.d.chatelain@kp.org>
2015-09-15 14:33:28 -0700

    Add /facilities path with mock response

--- locations.js
+++ locations.js
@@ -19,6 +19,22 @@ function initialize() {
   }
 }
 
+// KP facility data
+var sample = {
+    "facilities": [{
+        "lat": 34.043324,
+        "lng": -118.37654,
+        "name": "Vision Essentials by Kaiser Permanente, La Cienega",
+        "type": "Other",
+        "positive": 2,
+        "negative": 0
+    }]
+}
+
+function getKPLocations(zipcode, callback) {
+    callback(sample)
+}
+
 // express
 var app = express()
 app.set('port', process.env.PORT || 3000)
@@ -27,6 +43,20 @@ app.use(express.static(path.join(__dirname, 'public')))
 // set up paths
 app.get('/', function (request, response) {
     response.sendfile('index.html');
+})
+
+app.get('/facilities', function (request, response) {
+  var zipcode = request.query.zipcode
+
+  log.info('Request to find facilities for: ' + zipcode)
+
+  response.setHeader('Content-Type', 'application/json')
+
+  getKPLocations(zipcode, function (locationData) {
+    response.end(JSON.stringify({
+      facilities: locationData
+    }))
+  })
 })
 
 // start server
